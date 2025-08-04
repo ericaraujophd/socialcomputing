7e9dda0dde18f6f1c743c5ff7f56d507e2f68d0b
Eric Araujo <eric.araujo@calvin.edu>
2025-08-04 01:31:44 -0400

    changing to v2 of jupyter-book

--- deploy.sh
+++ deploy.sh
@@ -6,10 +6,10 @@ set -e  # Exit on error
 cd src
 
 if [[ $1 == "--all" ]]; then
-  jupyter-book clean . --all
-  jupyter-book build . --all
+  jupyter book clean . --all
+  jupyter book build . --all
 else
-  jupyter-book build .
+  jupyter book build .
 fi
 
 ghp-import -n -p -f _build/html
